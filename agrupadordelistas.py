e224d5657b5e313db5cf5842ad5a40f097f1f5e9
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-04-27 23:30:02 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,21 +1,29 @@
 import requests
 
-repo_urls = [    "https://github.com/strikeinthehouse/M3UPT/raw/main/M3U/M3UPT.m3u",    "https://api.github.com/repos/strikeinthehouse/M3UPT/contents",   "https://api.github.com/repos/ipstreet312/freeiptv/contents",  "https://api.github.com/repos/Nuttypro69/YouTube_to_m3u/contents"]
+repo_urls = [
+    "https://github.com/strikeinthehouse/M3UPT/raw/main/M3U/M3UPT.m3u",
+    "https://api.github.com/repos/strikeinthehouse/M3UPT/contents",
+    "https://api.github.com/repos/ipstreet312/freeiptv/contents",
+    "https://api.github.com/repos/Nuttypro69/YouTube_to_m3u/contents"
+]
 
 lists = []
 for url in repo_urls:
     response = requests.get(url)
 
     if response.status_code == 200:
-        contents = response.json()
-        m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
+        if url.endswith(".m3u"):
+            lists.append((url.split("/")[-1], response.text))
+        else:
+            contents = response.content.decode('utf-8')
+            m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
 
-        for m3u_file in m3u_files:
-            m3u_url = m3u_file["download_url"]
-            m3u_response = requests.get(m3u_url)
+            for m3u_file in m3u_files:
+                m3u_url = m3u_file["download_url"]
+                m3u_response = requests.get(m3u_url)
 
-            if m3u_response.status_code == 200:
-                lists.append((m3u_file["name"], m3u_response.text))
+                if m3u_response.status_code == 200:
+                    lists.append((m3u_file["name"], m3u_response.text))
     else:
         print(f"Error retrieving contents from {url}")
 
@@ -23,4 +31,4 @@ lists = sorted(lists, key=lambda x: x[0])
 
 with open("lista1.M3U", "w") as f:
     for l in lists:
-        f.write(l[1])            
+        f.write(l[1])
