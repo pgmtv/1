886a4b8f4ab4449359cfb473ad24823bbd1aee66
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-06-06 12:29:02 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,7 +1,13 @@
 import requests
 
 repo_urls = [
-    "https://api.github.com/repos/punkstarbr/STR-YT/contents"
+    "https://github.com/atsushi444/iptv-epg/raw/main/CCTV.m3u",
+    "https://github.com/iptv-org/iptv/raw/master/streams/mx_multimedios.m3u",
+    "https://github.com/strikeinthehouse/M3UPT/raw/main/M3U/M3UPT.m3u",
+    "https://api.github.com/repos/strikeinthehouse/YT2M3U/contents",
+    "https://api.github.com/repos/strikeinthehouse3/3/contents",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/fr.m3u",
+    "https://api.github.com/repos/Nuttypro69/YouTube_to_m3u/contents"
 ]
 
 lists = []
