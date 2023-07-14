53baa2750ce3a18dce0d64cd56a85e31637fad23
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-07-14 02:01:20 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,32 +1,44 @@
-from github import Github
+import requests
 
-# Token de acesso pessoal do GitHub (opcional)
-# Caso você esteja acessando um repositório privado, será necessário um token de acesso válido.
-# Caso contrário, deixe a variável 'access_token' como uma string vazia.
-access_token = ''
+url = "https://github.com/iptv-org/iptv/tree/master/streams"
 
-# URL do diretório do GitHub
-github_url = 'https://github.com/iptv-org/iptv/tree/master/streams'
+response = requests.get(url)
 
-# Extrai o nome do usuário e do repositório do URL
-github_user, github_repo = github_url.split('/')[3], github_url.split('/')[4]
+if response.status_code == 200:
+    contents = response.text
+    m3u_files = []
 
-# Cria uma instância do objeto Github
-if access_token:
-    g = Github(access_token)
+    # Extract .m3u file links from the response HTML
+    start_marker = '<td><a href="'
+    end_marker = '.m3u"'
+    start_index = contents.find(start_marker)
+
+    while start_index != -1:
+        end_index = contents.find(end_marker, start_index)
+        m3u_file_link = contents[start_index + len(start_marker):end_index + len(end_marker)]
+        m3u_files.append(m3u_file_link)
+        start_index = contents.find(start_marker, end_index)
+
+    lists = []
+
+    for m3u_file_link in m3u_files:
+        m3u_url = f"https://github.com{m3u_file_link}"
+        m3u_response = requests.get(m3u_url)
+
+        if m3u_response.status_code == 200:
+            lists.append((m3u_file_link.split("/")[-1], m3u_response.text))
+        else:
+            print(f"Error retrieving contents from {m3u_url}")
+
+    lists = sorted(lists, key=lambda x: x[0])
+
+    line_count = 0
+
+    with open("lista1.M3U", "w") as f:
+        for l in lists:
+            f.write(l[1])
+            line_count += l[1].count("\n")
+            if line_count >= 2000:  # Stop writing after 2000 lines
+                break
 else:
-    g = Github()
-
-# Obtém o repositório e o branch (ramo) principal
-repo = g.get_repo(f'{github_user}/{github_repo}')
-branch = repo.get_branch(repo.default_branch)
-
-# Obtém o conteúdo do diretório
-contents = repo.get_contents('', ref=branch.name)
-
-# Percorre o conteúdo do diretório
-for item in contents:
-    if item.type == 'file' and item.name.endswith('.m3u'):
-        playlist_url = item.download_url
-        print(playlist_url)
-
+    print(f"Error retrieving contents from {url}")
