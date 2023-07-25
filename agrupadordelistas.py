70fbd2a1f58d03032b4a82be9b90e8af0330eab0
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-07-25 10:19:57 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,14 +1,7 @@
 import requests
 
 repo_urls = [
-    "https://github.com/punkstarbr/STR-YT/raw/main/REALITY'SLIVE.m3u",
-    "https://github.com/atsushi444/iptv-epg/raw/main/CCTV.m3u",
-    "https://github.com/iptv-org/iptv/raw/master/streams/mx_multimedios.m3u",
-    "https://github.com/strikeinthehouse/M3UPT/raw/main/M3U/M3UPT.m3u",
-    "https://api.github.com/repos/strikeinthehouse/YT2M3U/contents",
-    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/fr.m3u",
-    "https://api.github.com/repos/Nuttypro69/YouTube_to_m3u/contents",
-    "https://api.github.com/repos/cqcbrasil/YouTube_to_m3u/contents"
+    "https://api.github.com/repos/punkstarbr/STR-YT/contents"
 ]
 
 lists = []
@@ -42,5 +35,5 @@ with open("lista1.M3U", "w") as f:
     for l in lists:
         f.write(l[1])
         line_count += l[1].count("\n")
-        if line_count >= 2010:  # Stop writing after 2010 lines
+        if line_count >= 2023:  # Stop writing after 2023 lines
             break
