d49c8e6fa1de512cae102b114bda312726307a8a
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-09-10 14:40:57 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -9,7 +9,7 @@ def is_within_time_range(start_time, end_time):
     return start_time <= current_time <= end_time
 
 # Horários locais do Brasil para 17h30 e 23h00
-start_time_br = datetime.now(brazil_timezone).replace(hour=17, minute=30, second=0, microsecond=0)
+start_time_br = datetime.now(brazil_timezone).replace(hour=11, minute=30, second=0, microsecond=0)
 end_time_br = datetime.now(brazil_timezone).replace(hour=23, minute=0, second=0, microsecond=0)
 
 if is_within_time_range(start_time_br, end_time_br):
