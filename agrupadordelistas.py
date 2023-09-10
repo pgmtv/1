a9b005a04a207398acdf10a57457e50356448278
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-09-10 14:30:16 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,4 +1,29 @@
 import requests
+from datetime import datetime, timezone, timedelta
+
+def is_within_time_range(start_time, end_time):
+    brazil_timezone = timezone(timedelta(hours=-3))  # Fuso horário do Brasil
+    current_time = datetime.now(brazil_timezone)
+    return start_time <= current_time <= end_time
+
+# Horários locais do Brasil para 17h30 e 23h00
+start_time_br = datetime.now().replace(hour=14, minute=30, second=0, microsecond=0)
+end_time_br = datetime.now().replace(hour=23, minute=0, second=0, microsecond=0)
+
+if is_within_time_range(start_time_br, end_time_br):
+    m3upt_url = "https://github.com/LITUATUI/M3UPT/raw/main/M3U/M3UPT.m3u"
+    m3upt_response = requests.get(m3upt_url)
+
+    if m3upt_response.status_code == 200:
+        m3upt_lines = m3upt_response.text.split('\n')[:10]
+
+        with open("lista1.M3U", "w") as f:
+            for line in m3upt_lines:
+                f.write(line + '\n')
+
+
+
+import requests
 
 repo_urls = [
     "https://github.com/punkstarbr/STR-YT/raw/main/lista1.m3u",
