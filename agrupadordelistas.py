74e7350d5aa5de80b58b6e9a2d2a41c5aec3f416
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2023-09-10 14:32:15 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -7,8 +7,8 @@ def is_within_time_range(start_time, end_time):
     return start_time <= current_time <= end_time
 
 # Horários locais do Brasil para 17h30 e 23h00
-start_time_br = datetime.now().replace(hour=14, minute=30, second=0, microsecond=0)
-end_time_br = datetime.now().replace(hour=23, minute=0, second=0, microsecond=0)
+start_time_br = datetime.now(brazil_timezone).replace(hour=14, minute=30, second=0, microsecond=0)
+end_time_br = datetime.now(brazil_timezone).replace(hour=23, minute=0, second=0, microsecond=0)
 
 if is_within_time_range(start_time_br, end_time_br):
     m3upt_url = "https://github.com/LITUATUI/M3UPT/raw/main/M3U/M3UPT.m3u"
@@ -20,6 +20,7 @@ if is_within_time_range(start_time_br, end_time_br):
         with open("lista1.M3U", "w") as f:
             for line in m3upt_lines:
                 f.write(line + '\n')
+
 
 
 
