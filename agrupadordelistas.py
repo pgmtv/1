1798180ea656848cf566bd00ddfa24398b3b1d22
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-07-14 00:42:58 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -79,7 +79,7 @@ ydl_opts = {
 }
 # Get the playlist and write to file
 try:
-    with open('./YOUTUBEPLAY1.m3u', 'w', encoding='utf-8') as f:
+    with open('./lista1.M3U', 'w', encoding='utf-8') as f:
         f.write("#EXTM3U\n")
         for i, link in enumerate(links):
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
