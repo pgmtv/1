a8a03411ea55190a0713c3b85798e9a9783c3757
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-07-14 12:18:02 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -96,3 +96,83 @@ try:
             f.write("\n")
 except Exception as e:
     print(f"Erro ao criar o arquivo .m3u8: {e}")
+
+
+
+import requests
+from datetime import datetime, timezone, timedelta
+
+
+
+
+
+# Defina o fuso horário do Brasil
+brazil_timezone = timezone(timedelta(hours=-3))
+
+def is_within_time_range(start_time, end_time):
+    current_time = datetime.now(brazil_timezone)
+    return start_time <= current_time <= end_time
+
+# Horários locais do Brasil para 17h30 e 23h00
+start_time_br = datetime.now(brazil_timezone).replace(hour=17, minute=30, second=0, microsecond=0)
+end_time_br = datetime.now(brazil_timezone).replace(hour=23, minute=0, second=0, microsecond=0)
+
+# Nome do arquivo de saída
+output_file = "lista1.M3U"
+
+if is_within_time_range(start_time_br, end_time_br):
+    m3upt_url = "https://github.com/LITUATUI/M3UPT/raw/main/M3U/M3UPT.m3u"
+    m3upt_response = requests.get(m3upt_url)
+
+    if m3upt_response.status_code == 200:
+        m3upt_lines = m3upt_response.text.split('\n')[:23]
+
+        with open(output_file, "a") as f:
+            for line in m3upt_lines:
+                f.write(line + '\n')
+else:
+    with open(output_file, "a") as f:
+        f.write("#EXTM3U\n")
+
+
+
+
+
+#GLOBO
+
+
+def is_within_time_range(start_time, end_time):
+    current_time = datetime.now(brazil_timezone)
+    return start_time <= current_time <= end_time
+
+# Horários locais do Brasil para 11h30 e 13h30
+start_time_br_morning = datetime.now(brazil_timezone).replace(hour=11, minute=30, second=0, microsecond=0)
+end_time_br_morning = datetime.now(brazil_timezone).replace(hour=13, minute=30, second=0, microsecond=0)
+
+# Horários locais do Brasil para 19h00 e 19h45
+start_time_br_evening = datetime.now(brazil_timezone).replace(hour=19, minute=0, second=0, microsecond=0)
+end_time_br_evening = datetime.now(brazil_timezone).replace(hour=19, minute=45, second=0, microsecond=0)
+
+# Horários locais do Brasil para 17h30 e 23h00
+start_time_br = datetime.now(brazil_timezone).replace(hour=5, minute=30, second=0, microsecond=0)
+end_time_br = datetime.now(brazil_timezone).replace(hour=8, minute=40, second=0, microsecond=0)
+
+# Nome do arquivo de saída
+output_file = "lista1.M3U"
+
+if (is_within_time_range(start_time_br_morning, end_time_br_morning) or 
+    is_within_time_range(start_time_br_evening, end_time_br_evening) or
+    is_within_time_range(start_time_br, end_time_br)):
+
+    m3upt_url = "https://gist.github.com/GUAPACHA/ff9cf6435b379c4c550913fdadc8edc4/raw/e5a4f0cc1ebe5a9cd07aa87c9be67004596db1e0/the%2520beatles"
+    m3upt_response = requests.get(m3upt_url)
+
+    if m3upt_response.status_code == 200:
+        m3upt_lines = m3upt_response.text.split('\n')[:422]
+
+        with open(output_file, "a") as f:
+            for line in m3upt_lines:
+                f.write(line + '\n')
+else:
+    with open(output_file, "a") as f:
+        f.write("#EXTM3U\n")
