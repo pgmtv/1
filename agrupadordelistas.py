9e080ce5785eadd90c094a0815c147977838a19b
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-07-20 09:24:27 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -19,7 +19,7 @@ chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
 # URL da página desejada
-url_youtube = "https://www.youtube.com/results?search_query=amlo&sp=EgJAAQ%253D%253D"
+url_youtube = "https://www.youtube.com/results?search_query=lopez+obrador&sp=EgJAAQ%253D%253D"
 
 # Abrir a página desejada
 driver.get(url_youtube)
