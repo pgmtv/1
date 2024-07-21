058e3f464ae72dcd98c84838856970230ed69e88
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-07-20 23:12:42 -0300

    Update agrupadordelistas40.py

--- agrupadordelistas40.py
+++ agrupadordelistas40.py
@@ -1,30 +1,51 @@
 import requests
-from datetime import datetime, timezone, timedelta
 
-# Defina o fuso horário do Brasil
-brazil_timezone = timezone(timedelta(hours=-3))
+repo_urls = [
+    "https://github.com/punkstarbr/STR-YT/raw/main/REALITY'SLIVE.m3u",
+    "https://github.com/iptv-org/iptv/blob/master/streams/mx.m3u",    
+    "https://github.com/iptv-org/iptv/raw/master/streams/jp.m3u",
+    "https://github.com/strikeinthehouse/Navez/raw/main/playlist.m3u",
+]
 
-def is_within_time_range(start_time, end_time):
-    current_time = datetime.now(brazil_timezone)
-    return start_time <= current_time <= end_time
 
-# Horários locais do Brasil para 17h30 e 23h00
-start_time_br = datetime.now(brazil_timezone).replace(hour=6, minute=00, second=0, microsecond=0)
-end_time_br = datetime.now(brazil_timezone).replace(hour=23, minute=59, second=0, microsecond=0)
 
-# Nome do arquivo de saída
-output_file = "lista1.M3U"
+lists = []
+for url in repo_urls:
+    response = requests.get(url)
 
-if is_within_time_range(start_time_br, end_time_br):
-    m3upt_url = "https://github.com/punkstarbr/STR-YT/raw/main/lista1.m3u"
-    m3upt_response = requests.get(m3upt_url)
+    if response.status_code == 200:
+        if url.endswith(".m3u"):
+            lists.append((url.split("/")[-1], response.text))
+        else:
+            try:
+                contents = response.json()
 
-    if m3upt_response.status_code == 200:
-        m3upt_lines = m3upt_response.text.split('\n')[:500]
+                m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
 
-        with open(output_file, "a") as f:
-            for line in m3upt_lines:
-                f.write(line + '\n')
-else:
-    with open(output_file, "a") as f:
-        f.write("#EXTM3U\n")
+                for m3u_file in m3u_files:
+                    m3u_url = m3u_file["download_url"]
+                    m3u_response = requests.get(m3u_url)
+
+                    if m3u_response.status_code == 200:
+                        lists.append((m3u_file["name"], m3u_response.text))
+            except requests.exceptions.JSONDecodeError:
+                print(f"Error parsing JSON from {url}")
+    else:
+        print(f"Error retrieving contents from {url}")
+
+lists = sorted(lists, key=lambda x: x[0])
+
+lists = sorted(lists, key=lambda x: x[0])
+
+line_count = 0
+with open("lista1.M3U", "a") as f:
+    for l in lists:
+        lines = l[1].split("\n")
+        for line in lines:
+            if line_count >= 212:
+                break
+            if line.strip():  # Pule linhas em branco
+                f.write(line + "\n")
+                line_count += 1
+        if line_count >= 200:
+            break
