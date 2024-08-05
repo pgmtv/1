73463280844330eef527c3b20d199ea8a8d9c4dc
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-05 02:53:36 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,98 +1,83 @@
-import subprocess
 import time
 import os
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import yt_dlp
 
+def configure_chrome_options():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    return chrome_options
 
+def initialize_driver(chrome_options):
+    return webdriver.Chrome(options=chrome_options)
 
+def fetch_page_source(driver, url, wait_time=5):
+    driver.get(url)
+    time.sleep(wait_time)  # Better to use WebDriverWait in real scenarios
+    return driver.page_source
 
-# Configuring Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+def scroll_to_bottom(driver, scroll_pause_time=2, scroll_count=5):
+    for _ in range(scroll_count):
+        driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+        time.sleep(scroll_pause_time)
 
-# Instanciando o driver do Chrome
-driver = webdriver.Chrome(options=chrome_options)
-
-# URL da página desejada
-url_youtube = "https://www.youtube.com/results?search_query=CNN+&sp=EgJAAQ%253D%253D"
-
-# Abrir a página desejada
-driver.get(url_youtube)
-
-# Aguardar alguns segundos para carregar todo o conteúdo da página
-time.sleep(5)
-
-from selenium.webdriver.common.keys import Keys
-for i in range(1):
-    try:
-        # Find the last video on the page
-        last_video = driver.find_element_by_xpath("//a[@class='ScCoreLink-sc-16kq0mq-0 jKBAWW tw-link'][last()]")
-        # Scroll to the last video
-        actions = ActionChains(driver)
-        actions.move_to_element(last_video).perform()
-        time.sleep(2)
-    except:
-        # Press the down arrow key for 50 seconds
-        driver.execute_script("window.scrollBy(0, 10000)")
-        time.sleep(2)
-        
-        
-# Get the page source again after scrolling to the bottom
-html_content = driver.page_source
-
-time.sleep(5)
-
-# Find the links and titles of the videos found
-try:
-    soup = BeautifulSoup(html_content, "html.parser")
+def extract_video_info(page_source):
+    soup = BeautifulSoup(page_source, "html.parser")
     videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
     links = ["https://www.youtube.com" + video.get("href") for video in videos]
     titles = [video.get("title") for video in videos]
-except Exception as e:
-    print(f"Erro: {e}")
-finally:
-    # Close the driver
-    driver.quit()
+    return links, titles
 
+def create_m3u_playlist(links, filename='./lista1.M3U'):
+    ydl_opts = {
+        'format': 'best',
+        'write_all_thumbnails': False,
+        'skip_download': True,
+    }
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write("#EXTM3U\n")
+            for link in links:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(link, download=False)
+                if 'url' not in info:
+                    print(f"Erro ao gravar informações do vídeo {link}: 'url'")
+                    continue
+                url = info['url']
+                thumbnail_url = info.get('thumbnail', '')
+                description = info.get('description', '')[:10]
+                title = info.get('title', '')
+                f.write(f"#EXTINF:-1 group-title=\"YOUTUBE\" tvg-logo=\"{thumbnail_url}\",{title} - {description}...\n")
+                f.write(f"{url}\n")
+                f.write("\n")
+    except Exception as e:
+        print(f"Erro ao criar o arquivo .m3u: {e}")
 
+def main():
+    url_youtube = "https://www.youtube.com/results?search_query=CNN+&sp=EgJAAQ%253D%253D"
+    
+    chrome_options = configure_chrome_options()
+    driver = initialize_driver(chrome_options)
+    
+    try:
+        page_source = fetch_page_source(driver, url_youtube)
+        scroll_to_bottom(driver)
+        page_source = driver.page_source  # Get the page source again after scrolling
 
+        links, _ = extract_video_info(page_source)
+        create_m3u_playlist(links)
 
+    finally:
+        driver.quit()
 
+if __name__ == "__main__":
+    main()
 
-time.sleep(5)
-
-
-# Define as opções para o youtube-dl
-ydl_opts = {
-    'format': 'best',  # Obtém a melhor qualidade
-
-    'write_all_thumbnails': False,  # Não faz download das thumbnails
-    'skip_download': True,  # Não faz download do vídeo
-}
-# Get the playlist and write to file
-try:
-    with open('./lista1.M3U', 'w', encoding='utf-8') as f:
-        f.write("#EXTM3U\n")
-        for i, link in enumerate(links):
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(link, download=False)
-            if 'url' not in info:
-                print(f"Erro ao gravar informações do vídeo {link}: 'url'")
-                continue
-            url = info['url']
-            thumbnail_url = info['thumbnail']
-            description = info.get('description', '')[:10]
-            title = info.get('title', '')
-            f.write(f"#EXTINF:-1 group-title=\"YOUTUBE\" tvg-logo=\"{thumbnail_url}\",{title} - {description}...\n")
-            f.write(f"{url}\n")
-            f.write("\n")
-except Exception as e:
-    print(f"Erro ao criar o arquivo .m3u8: {e}")
 
 
 
