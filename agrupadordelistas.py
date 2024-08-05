12012b9d6e626ce104ccee2da2f18180c1d4d40f
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-05 04:29:49 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -39,8 +39,6 @@ def get_video_metadata(video_url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
             info = ydl.extract_info(video_url, download=False)
-            formats = info.get('formats', [])
-            best_format = max(formats, key=lambda x: x.get('height', 0), default={})
             url = best_format.get('url', '')
             title = info.get('title', 'No Title')
             description = info.get('description', '')[:10]
