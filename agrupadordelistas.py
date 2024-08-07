8869aac7c5cec5859820c425a0071b244f7cd583
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-07 01:25:51 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -502,7 +502,7 @@ else:
     print('Nenhum conteúdo de arquivo .m3u foi encontrado para escrever.')
 
 
-def limitar_arquivo_m3u(arquivo_original, arquivo_saida, limite_linhas=700):
+def limitar_arquivo_m3u(arquivo_original, arquivo_saida, limite_linhas=7000):
     try:
         # Abre o arquivo M3U original para leitura
         with open(arquivo_original, 'r') as file:
