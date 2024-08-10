4da478c9a56b00b8ac3d0ea5f4a39c690235b819
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-10 04:37:04 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -19,7 +19,7 @@ options.add_argument("--disable-infobars")
 driver = webdriver.Chrome(options=options)
 
 # URL of the desired page
-url_archive = "https://archive.org/details/tvarchive?query=tonight+show&sort=-date"
+url_archive = "https://archive.org/details/tvarchive?query=paris&sort=-date"
 
 # Open the desired page
 driver.get(url_archive)
