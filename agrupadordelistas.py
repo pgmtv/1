fca23dde523b44a5691e76af625cf7915f9c9542
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-15 09:19:32 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 
 # Configure Chrome options
 options = Options()
+options.add_argument("-headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1280,720")
