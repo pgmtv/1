68fea5f85ddb37d412cab2d380f1ac4b4b1f65b4
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-27 16:45:27 -0300

    Update pt.py

--- pt.py
+++ pt.py
@@ -39,46 +39,6 @@ with open('pt.txt', 'w') as file:
 # Close the driver
 driver.quit()
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import time
-
-# Configure Chrome options
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1280,720")
-options.add_argument("--disable-infobars")
-
-# Create the webdriver instance
-driver = webdriver.Chrome(options=options)
-
-# URL of the desired page
-url_archive = "https://tviplayer.iol.pt/direto/TVI"
-
-# Open the desired page
-driver.get(url_archive)
-
-# Wait for the page to load
-time.sleep(5)  # Adjust the sleep time if needed to ensure page load
-
-# Find all relevant video links
-# Adjust the selector to target the a tags within the navCanaisTopo class
-video_elements = driver.find_elements(By.CSS_SELECTOR, 'div.navCanaisTopo a')
-
-# Prepare to write the links to a file
-with open('pt.txt', 'a') as file:
-    for element in video_elements:
-        link = element.get_attribute('href')
-        # Check if the link is valid and not empty
-        if link:
-            full_link = f"{link}"  # Construct the full URL
-            file.write(full_link + '\n')
-
-# Close the driver
-driver.quit()
 
 
 import subprocess
