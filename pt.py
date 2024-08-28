c13db23bc02032d1f0b0cd2c7e29712d3e93224e
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2024-08-28 02:40:33 -0300

    Update pt.py

--- pt.py
+++ pt.py
@@ -57,7 +57,7 @@ def write_m3u_file(links, output_path):
                     # Adiciona a entrada no arquivo M3U
                     f.write(f"{link}\n")
 
-url = "https://megacanaisonline.com/estreladacasa.php"
+url = "https://vimeo.com/search/sort:latest?q=aula"
 driver.get(url)
 
 for i in range(2):
