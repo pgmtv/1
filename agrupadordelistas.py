44e53ed4f494048597e28fe6d9837eef903616fa
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-01-23 23:56:51 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -26,10 +26,10 @@ driver.get(url_archive)
 # Wait for the page to load
 time.sleep(5)
 
-# Scroll to the bottom of the page
-#for _ in range(1):
-#    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#    time.sleep(2)
+ Scroll to the bottom of the page
+for _ in range(10):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
 
 # Find all elements with the specified <a> tags
 elements = driver.find_elements(By.CSS_SELECTOR, 'a[title][data-event-click-tracking="GenericNonCollection|ItemTile"]')
