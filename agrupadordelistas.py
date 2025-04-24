190f520352911386e0df8b67c3e7fb032d844c9d
strikeinthehouse <132027324+strikeinthehouse@users.noreply.github.com>
2025-04-24 02:51:26 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -4,8 +4,8 @@ import requests
 # URLs dos repositórios que contêm os arquivos M3U
 repo_urls = [
     "https://github.com/strikeinthehouse/1/raw/refs/heads/main/lista_1.M3U",
-    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/mx.m3u",    
     "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/ve.m3u",
+    "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/mx.m3u",        
     "https://github.com/strikeinthehouse/Navez/raw/main/playlist.m3u",
 ]
 
